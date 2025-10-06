02ff578b941279843715a066d15eb00cad333647
Imperor <119866946+Imperor0103@users.noreply.github.com>
2025-10-07 08:25:04 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 84 ms, Memory: 15616 KB -BaekjoonHub

---
+++ 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.cc
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+
+void DFS(vector<vector<int>>* graph, vector<int>* visited, int* order, int node)
+{
+	(*visited)[node] = (*order)++;	// 현재 노드 방문 순서 기록
+
+	for (int i = 0; i < (*graph)[node].size(); i++)
+	{
+		int next = (*graph)[node][i];
+		if ((*visited)[next] == 0)
+			DFS(graph, visited, order, next);
+	}
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int N, M, R;
+	cin >> N >> M >> R;
+
+	vector<vector<int>> graph(N + 1);	// 인접 리스트
+	vector<int> visited(N + 1, 0);	// 방문 순서 저장
+	int order = 1;
+
+	for (int i = 0; i < M; i++)
+	{
+		int u, v;
+		cin >> u >> v;
+
+		// 무방향그래프이므로 간선 정보를 양쪽 모두 추가
+		graph[u].push_back(v);
+		graph[v].push_back(u);
+	}
+
+	for (int i = 1; i <= N; i++)
+	{
+		// 방문 순서를 내림차순으로 정렬
+		sort(graph[i].begin(), graph[i].end(), greater<int>());
+	}
+
+	DFS(&graph, &visited, &order, R);
+
+	for (int i = 1; i <= N; i++)
+		cout << visited[i] << '\n';
+
+	return 0;
+}
